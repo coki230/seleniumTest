9c851c1f5ed95b2004a6396afe206932eaebcad9
John Doe <john@sample.com>
2020-11-07 20:15:55 +0800

    tapd上传

--- src/main/java/SeleTest.java
+++ src/main/java/SeleTest.java
@@ -1,23 +1,32 @@
+import com.alibaba.fastjson.JSON;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,40 +34,165 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class SeleTest {
-    public static void main(String[] args) throws InterruptedException, IOException {
+    public static void main(String[] args) {
         SeleTest seleTest = new SeleTest();
         System.setProperty("webdriver.gecko.driver", "E:\\soft\\geckodriver/geckodriver.exe");
-
         WebDriver driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        try {
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        seleTest.getProject(driver);
+            seleTest.tapc(driver);
 
-        driver.close();
+            driver.close();
+            driver.quit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            driver.close();
+            driver.quit();
+        }
     }
 
-    private void tapc(WebDriver driver) throws InterruptedException {
+    private void tapc(WebDriver driver) throws InterruptedException, IOException {
 
         driver.get("https://www.tapd.cn/51894879/bugtrace/bugreports/my_view");
-        Thread.sleep(20000);
-
+        setCookie(driver);
+        driver.navigate().refresh();
+        Thread.sleep(2000);
+        createTapd(driver);
 
     }
 
-    private void getProject(WebDriver driver) throws IOException {
+    private void hoverAndClick(WebDriver driver, WebElement webElement) {
+        Actions actions = new Actions(driver);
+        actions.moveToElement(webElement).clickAndHold().click().perform();
+    }
+
+    private void createTapd(WebDriver driver) throws InterruptedException {
+        // create new button
+        driver.findElement(By.id("btn_add_bug")).click();
+
+        String mainWindow = driver.getWindowHandle();
+        Set<String> windowHandles = driver.getWindowHandles();
+        for (String windowHandle : windowHandles) {
+            WebDriver window = driver.switchTo().window(windowHandle);
+            Thread.sleep(200);
+            if (window.getCurrentUrl().contains("add")) {
+//                // set title
+//                driver.findElement(By.id("BugTitle")).clear();
+//                driver.findElement(By.id("BugTitle")).sendKeys("aaa");
+//                // set 优先级
+//                WebElement bugPriority = driver.findElement(By.xpath("//*[@id=\"BugPriorityDiv\"]/div"));
+//                bugPriority.click();
+//                hoverAndClick(driver, driver.findElement(By.xpath("//td[@title='中']")));
+//                // set 严重程度 *
+//                WebElement bugSeverity = driver.findElement(By.xpath("//*[@id=\"BugSeverityDiv\"]/div/div"));
+//                bugSeverity.click();
+//                hoverAndClick(driver, driver.findElement(By.xpath("//td[@title='一般']")));
+//                // set 混合云环境 *
+//                WebElement env = driver.findElement(By.xpath("//*[@id=\"BugCustomFieldThreeDiv\"]/div/div"));
+//                env.click();
+//                hoverAndClick(driver, driver.findElement(By.xpath("//td[@title='dev']")));
+//                // set 处理人
+//                WebElement bugCurrentOwner = driver.findElement(By.id("BugCurrentOwnerValue"));
+//                bugCurrentOwner.click();
+//                bugCurrentOwner.sendKeys("肖");
+//                Thread.sleep(2000);
+//                // we need click some element to full the input
+//                driver.findElement(By.id("BugTitle")).click();
+//                // set 所属部门 *
+//                WebElement bugCustomFieldOne = driver.findElement(By.xpath("//*[@id=\"BugCustomFieldOneDiv\"]/div/div"));
+//                bugCustomFieldOne.click();
+//                hoverAndClick(driver, driver.findElement(By.xpath("//td[@title='行业应用-研发五部']")));
+//                // set 业务范围 *
+//                WebElement bugCustomFieldFive = driver.findElement(By.xpath("//*[@id=\"BugCustomFieldFiveDiv\"]/div/div"));
+//                bugCustomFieldFive.click();
+//                hoverAndClick(driver, driver.findElement(By.xpath("//td[@title='共享交换']")));
+//                // set 项目标识 *
+//                driver.findElement(By.id("BugCustomFieldTwo")).sendKeys("aaa");
+//                // set 问题分类 *
+//                WebElement BugCustomField6 = driver.findElement(By.xpath("//*[@id=\"BugCustomField6Div\"]/div/div"));
+//                BugCustomField6.click();
+//                hoverAndClick(driver, driver.findElement(By.xpath("//td[@title='功能缺陷']")));
+
+//                driver.switchTo().frame(driver.findElement(By.xpath("//*[@id=\"editor-BugDescription\"]/div[7]/iframe")));
+//                JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+//                // 实施人员
+//                jsExecutor.executeScript("arguments[0].innerHTML = '赵泽'", driver.findElement(By.xpath("/html/body/div[1]/div[1]/table/tbody/tr[4]/td[1]/div")));
+//                // 实施时间
+//                Date t = new Date();
+//                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//                jsExecutor.executeScript("arguments[0].innerHTML = '"+ df.format(t) +"'", driver.findElement(By.xpath("/html/body/div[1]/div[1]/table/tbody/tr[4]/td[2]/div")));
+//                // 实施事项
+//                jsExecutor.executeScript("arguments[0].innerHTML = '大盘巡检'", driver.findElement(By.xpath("/html/body/div[1]/div[1]/table/tbody/tr[4]/td[3]/div")));
+//                // 问题截图
+//                jsExecutor.executeScript("arguments[0].innerHTML = 'url'", driver.findElement(By.xpath("/html/body/div[1]/div[3]")));
+//                jsExecutor.executeScript("arguments[0].focus()", driver.findElement(By.xpath("/html/body/div[1]/div[3]")));
+
+                driver.switchTo().window(windowHandle);
+                driver.findElement(By.xpath("//*[@id=\"upload-attachement\"]/b")).click();
+
+                System.out.println(111);
+
+
+            }
+        }
+    }
+
+    private void setCookie(WebDriver driver) throws IOException {
+        driver.manage().deleteAllCookies();
+        URL a = SeleTest.class.getClassLoader().getResource("");
+        File file1 = new File(a.getPath() + "\\cookie.txt");
+        BufferedReader reader = new BufferedReader(new FileReader(file1));
+        StringBuffer buffer = new StringBuffer();
+        String s = "";
+        while ((s = reader.readLine()) != null) {
+            buffer.append(s);
+        }
+        List<Cookie> cookieList = JSON.parseArray(buffer.toString(), Cookie.class);
+        for (Cookie cookie : cookieList) {
+            driver.manage().addCookie(cookie);
+        }
+        reader.close();
+    }
+
+    private void writeCookie(WebDriver driver) throws IOException {
+        System.out.println("start writeCookie");
+        Set<Cookie> cookies = driver.manage().getCookies();
+        System.out.println(cookies.size());
+        for (Cookie cookie : cookies) {
+            System.out.println(cookie);
+        }
+        String s = JSON.toJSONString(cookies);
+        saveData(s, "cookie.txt");
+    }
+
+    private void saveData(String data, String file) throws IOException {
+        URL a = SeleTest.class.getClassLoader().getResource("");
+        File file1 = new File(a.getPath() + "\\" + file);
+        file1.createNewFile();
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file1));
+        writer.write(data);
+        writer.flush();
+        writer.close();
+    }
+
+    private void getProject(WebDriver driver) throws IOException, InterruptedException {
         // dev test prod
-        String env = "prod";
+        String env = "test";
         Map<String, Relation> projectInfo = getProjectInfo();
 
         driver.get("https://dolphin-"+ env +".kedacom.com/dashboard/#/service/dashboard");
 
         // type is error or slow
-        getInfo(driver, "error", projectInfo, env);
+        List<Tapd> tapdList = getInfo(driver, "error", projectInfo, env);
+        String s = JSON.toJSONString(tapdList);
+        saveData(s, "tapdList.txt");
     }
 
     private Map<String, Relation> getProjectInfo() throws IOException {
         Map<String, Relation> relationMap = new HashMap<>();
-        BufferedReader reader = new BufferedReader(new FileReader(new File("D:\\learn\\seleniumTest\\src\\main\\java\\info.txt")));
+        URL a = SeleTest.class.getClassLoader().getResource("info.txt");
+        BufferedReader reader = new BufferedReader(new FileReader(new File(a.getFile())));
         String s = null;
         while ((s = reader.readLine()) != null) {
             Relation relation = new Relation();
@@ -72,7 +206,8 @@ public class SeleTest {
         return relationMap;
     }
 
-    private void getInfo(WebDriver driver, String type, Map<String, Relation> project, String env) throws IOException {
+    private List<Tapd> getInfo(WebDriver driver, String type, Map<String, Relation> project, String env) throws IOException, InterruptedException {
+        List<Tapd> tapds = new ArrayList<>();
         List<String> errorMarkAndNamespaceList = new ArrayList<>();
         // find apm
         driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div[2]/section/div/ul/li[3]")).click();
@@ -88,12 +223,16 @@ public class SeleTest {
 
         String mainWindow = driver.getWindowHandle();
         // click the error project to apm page
-        for (WebElement element : elements) {
+        for (int i = 0; i < elements.size(); i++) {
             // enter detail page
-            WebElement detailPage = element.findElement(By.cssSelector("span.can-click"));
-            //使用显示等待，等待掩盖的div消失
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-            wait.until(ExpectedConditions.elementToBeClickable(detailPage));
+            WebElement detailPage = elements.get(i).findElement(By.cssSelector("span.can-click"));
+
+            if (i == 0) {
+                Thread.sleep(3000);
+                //使用显示等待，等待掩盖的div消失
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+                wait.until(ExpectedConditions.elementToBeClickable(detailPage));
+            }
 
             detailPage.click();
 
@@ -140,6 +279,7 @@ public class SeleTest {
 
         // print tapd info
         for (String s : errorMarkAndNamespaceList) {
+            Tapd tapd = new Tapd();
             String[] split = s.split(";");
             Relation relation = project.get(split[1]);
             System.out.println("【"+ env + "环境】"+ split[0] +" + "+ split[1] +" + 有错误");
@@ -149,13 +289,22 @@ public class SeleTest {
             System.out.println("业务范围 " + relation.getBelong());
             System.out.println("问题链接 " + split[2]);
             System.out.println();
+            tapd.setDealMan(relation.getOwnerName());
+            tapd.setDept(relation.getDept());
+            tapd.setLink(split[2]);
+            tapd.setMark(split[0]);
+            tapd.setRange(relation.getBelong());
+            tapd.setTitle("【"+ env + "环境】"+ split[0] +" + "+ split[1] +" + 有错误");
+            tapd.setNamespace(split[0]);
+            tapds.add(tapd);
         }
+        return tapds;
     }
 
     private File captureElement(File screenshot, WebElement element, WebDriver driver){
         try {
             // wait to get all info
-            Thread.sleep(3000);
+            Thread.sleep(6000);
             File srcfile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
             FileUtils.copyFile(srcfile, screenshot);
             BufferedImage img = ImageIO.read(screenshot);
---
+++ src/main/java/Tapd.java
@@ -0,0 +1,66 @@
+public class Tapd {
+    private String title;
+    private String dealMan;
+    private String dept;
+    private String range;
+    private String mark;
+    private String namespace;
+
+    public String getNamespace() {
+        return namespace;
+    }
+
+    public void setNamespace(String namespace) {
+        this.namespace = namespace;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    private String link;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDealMan() {
+        return dealMan;
+    }
+
+    public void setDealMan(String dealMan) {
+        this.dealMan = dealMan;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public String getRange() {
+        return range;
+    }
+
+    public void setRange(String range) {
+        this.range = range;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+}
